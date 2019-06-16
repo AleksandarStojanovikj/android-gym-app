52c2fca33644de5672ee7950dc8ab09b16f54d54
Marija Trajkova <tmarija2008@yahoo.com>
2019-06-16 16:51:00 +0200

    Fixed menu

--- mobile/app/src/main/java/com/gymity/ProductGridFragment.java
+++ mobile/app/src/main/java/com/gymity/ProductGridFragment.java
@@ -3,6 +3,8 @@ package com.gymity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
@@ -14,9 +16,18 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.button.MaterialButton;
 import com.gymity.network.ProductEntry;
 
 public class ProductGridFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    MaterialButton gymButton;
 
     @Override
     public View onCreateView(
@@ -24,6 +35,8 @@ public class ProductGridFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
         setUpToolbar(view);
+
+        gymButton = view.findViewById(R.id.gyms_button);
 
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
@@ -45,6 +58,13 @@ public class ProductGridFragment extends Fragment {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             view.findViewById(R.id.product_grid).setBackgroundResource(R.drawable.shr_product_grid_background_shape);
         }
+
+        gymButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((NavigationHost) getActivity()).navigateTo(new GymsFragment(), true);
+            }
+        });
         return view;
     }
 
@@ -58,7 +78,15 @@ public class ProductGridFragment extends Fragment {
         toolbar.setNavigationOnClickListener(new NavigationIconClickListener(
                 getContext(),
                 view.findViewById(R.id.product_grid),
-                new AccelerateDecelerateInterpolator()));
+                new AccelerateDecelerateInterpolator(),
+                getContext().getResources().getDrawable(R.drawable.shr_menu),
+                getContext().getResources().getDrawable(R.drawable.shr_close_menu)));
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.shr_toolbar_menu, menu);
+        super.onCreateOptionsMenu(menu, menuInflater);
     }
 
 }
