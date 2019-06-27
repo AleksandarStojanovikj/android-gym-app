19f470b32875bc49f8022e33b787879424bb16bd
Aleksandar Stojanovikj <aleksandar_sangi@hotmail.com>
2019-06-27 10:52:24 +0200

    Make username unique

--- api/src/main/java/com/gymity/project/model/Credentials.java
+++ api/src/main/java/com/gymity/project/model/Credentials.java
@@ -1,9 +1,11 @@
 package com.gymity.project.model;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class Credentials {
+    @Column(unique = true)
     public String username;
     public String password;
 }
